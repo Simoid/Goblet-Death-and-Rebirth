70ba70ed61b272ed0183c6354c9f3f8cf7038cba
Johan Ribom <jribom@kth.se>
2016-05-04 14:50:41 +0200

    La till eclipse-projekt med en första javafil.

---
+++ Goblet: Death and Rebirth/src/Engine/GameEngine.java
@@ -0,0 +1,16 @@
+package Engine;
+
+
+/**
+ * Main class for the game engine.
+ * @author johan
+ *
+ */
+public class GameEngine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Engine/GameEngine.java
+++
@@ -1,1 +0,0 @@
-// Main game engine file
