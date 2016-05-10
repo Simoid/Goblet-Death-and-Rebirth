71912d7b84c771fe2073de93d9002fba56081753
Johan Ribom <jribom@kth.se>
2016-05-10 15:02:29 +0200

    Första grafiktest med libgdx.

--- Goblet - Death and Rebirth-core/src/com/mygdx/game/Engine.java
+++ Goblet - Death and Rebirth-core/src/com/mygdx/game/Engine.java
@@ -6,10 +6,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Engine extends ApplicationAdapter {
 	private SpriteBatch batch;
+	private Texture texture;
+	private Sprite sprite;
 	private BitmapFont font;
 	
 	@Override
@@ -17,6 +20,8 @@ public class Engine extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
+		texture = new Texture(Gdx.files.internal("sprites/goblet_MC_new.png"));
+		sprite = new Sprite(texture);
 	}
 	
 	@Override
@@ -29,8 +34,10 @@ public class Engine extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
 		font.draw(batch, "Hello world!", 200, 200);
+		sprite.draw(batch);
 		batch.end();
 	}
 	
--- Goblet - Death and Rebirth-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Goblet - Death and Rebirth-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.mygdx.game.Engine;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Goblet: Death and Rebirth";
+		config.backgroundFPS = 12;
+		config.foregroundFPS = 60;
+		config.fullscreen = true;
+		config.width = 1920;
+		config.height = 1080;
 		new LwjglApplication(new Engine(), config);
 	}
 }
