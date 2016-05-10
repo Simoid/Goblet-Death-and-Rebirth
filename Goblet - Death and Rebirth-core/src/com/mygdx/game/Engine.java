340d5ed2c037c663424b88e41a42ce797c9208dc
Johan Ribom <jribom@kth.se>
2016-05-10 14:19:25 +0200

    Ett första hello world med libgdx.

--- Goblet - Death and Rebirth-core/src/com/mygdx/game/Engine.java
+++ Goblet - Death and Rebirth-core/src/com/mygdx/game/Engine.java
@@ -2,26 +2,43 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Engine extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.RED);
 	}
-
+	
+	@Override
+	public void dispose(){
+		batch.dispose();
+		font.dispose();
+	}
+	
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello world!", 200, 200);
 		batch.end();
 	}
+	
+	@Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
 }
