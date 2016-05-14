9159a660a3d8e7df8ac4b06fd32eedd406a89475
Johan Ribom <johan.ribom@hotmail.com>
2016-05-15 00:52:05 +0200

    Skapade ett paket för entitites med en playerklass.

---
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/entities/Player.java
@@ -0,0 +1,13 @@
+package com.goblet.entities;
+
+import com.goblet.graphics.SpriteAnimation;
+
+/**
+ * Created by Johan on 2016-05-14.
+ */
+public class Player {
+
+    SpriteAnimation walkDown;
+
+
+}
