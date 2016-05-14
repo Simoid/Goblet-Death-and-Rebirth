840a68a8acb55b4d70eb7194ea67ec6e1f04c226
Johan Ribom <jribom@kth.se>
2016-05-14 16:23:01 +0200

    Skapade en SpriteAnimation-klass för att kunna scala animationer.

--- Goblet-Death-and-Rebirth/core/src/com/goblet/gameEngine/Engine.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/gameEngine/Engine.java
@@ -6,15 +6,14 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.goblet.graphics.SpriteAnimation;
 
 public class Engine extends ApplicationAdapter {
 	//TEST COMMENT!
 	private SpriteBatch batch;
 	private BitmapFont font;
-	private TextureAtlas atlas;
-	private Animation animation;
 	private float elapsedTime = 0f;
-	private TextureRegion[] frontwalk = new TextureRegion[4];
+	private SpriteAnimation mc_walk;
 	private float mc_x = 0;
 	private float mc_y = 0;
 
@@ -23,19 +22,13 @@ public class Engine extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
-		atlas = new TextureAtlas(Gdx.files.internal("assets/sprites/mc_frontwalk.pack"));
-		frontwalk[0] = (atlas.findRegion("spr_mc_frontwalk00"));
-		frontwalk[1] = (atlas.findRegion("spr_mc_frontwalk01"));
-		frontwalk[2] = (atlas.findRegion("spr_mc_frontwalk02"));
-		frontwalk[3] = (atlas.findRegion("spr_mc_frontwalk03"));
-		animation = new Animation(1/8f, frontwalk);
+		mc_walk = new SpriteAnimation("assets/sprites/mc_frontwalk.pack", 4, 2.5f, 1/5f);
 	}
 
 	@Override
 	public void dispose(){
 		batch.dispose();
 		font.dispose();
-		atlas.dispose();
 	}
 
 	@Override
@@ -47,33 +40,33 @@ public class Engine extends ApplicationAdapter {
 			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				mc_x -= 1f;
 			else
-				mc_x -= 5.0f;
+				mc_x -= 3.0f;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
 			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				mc_x += 1f;
 			else
-				mc_x += 5.0f;
+				mc_x += 3.0f;
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
 			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				mc_y -= 1f;
 			else
-				mc_y -= 5.0f;
+				mc_y -= 3.0f;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.UP)){
 			if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
 				mc_y += 1f;
 			else
-				mc_y += 5.0f;
+				mc_y += 3.0f;
 		}
 
 
 		batch.begin();
 		font.draw(batch, "Hello world!", 200, 200);
 		elapsedTime += Gdx.graphics.getDeltaTime();
-		batch.draw(animation.getKeyFrame(elapsedTime, true), mc_x, mc_y);
+		mc_walk.draw(batch, mc_x, mc_y, elapsedTime);
 		batch.end();
 	}
 
--- Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/SpriteAnimation.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/SpriteAnimation.java
@@ -1,7 +1,28 @@
 package com.goblet.graphics;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.*;
+
 /**
  * Created by johan on 5/14/16.
  */
 public class SpriteAnimation {
+    private TextureRegion[] regions;
+    private float scale;
+    private Animation animation;
+
+    public SpriteAnimation(String atlasLocation, int numberOfSprites, float scale, float animationTime){
+        this.scale = scale;
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(atlasLocation));
+        regions = new TextureRegion[numberOfSprites];
+        for (int i = 0; i < numberOfSprites; i++){
+            regions[i] = atlas.findRegion(String.format("%04d", i));
+        }
+        animation = new Animation(animationTime, regions);
+    }
+
+    public void draw(Batch batch, float x, float y, float time){
+        TextureRegion region = animation.getKeyFrame(time, true);
+        batch.draw(region, x, y, region.getRegionWidth()*scale, region.getRegionHeight()*scale);
+    }
 }
