520d4762b9612fb181da7b287760374b7e4930bd
Johan Ribom <johan.ribom@hotmail.com>
2016-05-15 00:50:39 +0200

    Kommenterade all kod i klassen.

--- Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/SpriteAnimation.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/SpriteAnimation.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.*;
 
 /**
+ * En klass som innehåller en animation och kan rita ut den större eller mindre. Klassen kräver en spritesheet
+ * och en .atlas-fil som beskriver spritesheetet, samt att alla bilder i atlas-filen är döpta från 0000.png och
+ * uppåt.
  * Created by johan on 5/14/16.
  */
 public class SpriteAnimation {
@@ -11,8 +14,15 @@ public class SpriteAnimation {
     private float scale;
     private Animation animation;
 
-    public SpriteAnimation(String atlasLocation, int numberOfSprites, float scale, float animationTime){
-        this.scale = scale;
+    /**
+     * Konstruktur för klassen.
+     * @param atlasLocation Filvägen till atlas filen (oftast "assets/sprites/***.pack")
+     * @param numberOfSprites Hur många bilder som ingår i animationen.
+     * @param scale Storleken på bilden.
+     * @param animationTime Tidsskillnaden mellan bildbyte.
+     */
+    public SpriteAnimation(String atlasLocation, int numberOfSprites, int scale, float animationTime){
+        this.scale = (float) scale;
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal(atlasLocation));
         regions = new TextureRegion[numberOfSprites];
         for (int i = 0; i < numberOfSprites; i++){
@@ -21,6 +31,24 @@ public class SpriteAnimation {
         animation = new Animation(animationTime, regions);
     }
 
+    /**
+     * Funktion för att sätta storleken på bilden som ritas ut.
+     * Parametern är en int som sedan görs om till float, detta för att se till att alla pixlar på karaktären
+     * ritas ut. Om en float kunde skickas in och t.ex. 2.5f skickas in kommer inte bilden att se symmetrisk
+     * ut, därför får bilden endast skaleras med en int.
+     * @param scale Hur mycket bilden skaleras.
+     */
+    public void setScale(int scale){
+        this.scale = (float) scale;
+    }
+
+    /**
+     * Rita ut animationen.
+     * @param batch Batchen som animationen ska ritas ut på.
+     * @param x X-koordinaten för animationen.
+     * @param y Y-koordinaten för animationen.
+     * @param time Tiden som har passerat sen animationen började.
+     */
     public void draw(Batch batch, float x, float y, float time){
         TextureRegion region = animation.getKeyFrame(time, true);
         batch.draw(region, x, y, region.getRegionWidth()*scale, region.getRegionHeight()*scale);
