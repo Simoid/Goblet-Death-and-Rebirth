b1ea3c321428c87a4b3e28310baa6ae46a5ae37a
Johan Ribom <johan.ribom@hotmail.com>
2016-05-23 16:23:51 +0200

    Började med RoomParsers konstruktor.

--- Goblet-Death-and-Rebirth/core/src/com/goblet/gameEngine/RoomParser.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/gameEngine/RoomParser.java
@@ -1,8 +1,32 @@
 package com.goblet.gameEngine;
 
-import javax.json.*;
+import com.goblet.level.Tile;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+
 /**
  * Created by Simoido on 2016-05-19.
  */
 public class RoomParser {
+
+    Reader reader;
+    JsonElement je;
+
+    public RoomParser(String roomName, String fileName, Tile[][] tiles){
+        try {
+            reader = new FileReader(fileName);
+            je = new JsonParser().parse(reader);
+            JsonObject roomObject = je.getAsJsonObject().get(roomName).getAsJsonObject();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
