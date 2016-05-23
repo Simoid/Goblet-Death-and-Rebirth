2d08f9eff3b670e8aeb1d4b92041e19cdb40a69c
Johan Ribom <johan.ribom@hotmail.com>
2016-05-23 18:59:21 +0200

    La till WallObject.java som inte följde med i förra commiten.

---
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/level/wallObject.java
@@ -0,0 +1,50 @@
+package com.goblet.level;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.goblet.entities.Direction;
+import com.goblet.level.Position;
+
+/**
+ * Created by Johan on 2016-05-23.
+ */
+public class WallObject {
+
+    private TextureRegion region;
+    private Direction dir;
+    private Position position;
+    private Position bottomLeft;
+    private Position topRight;
+
+    public WallObject(TextureRegion region, TextureRegion regionWithDoor, Direction dir, Position bottomLeft, Position topRight){
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+        this.region = region;
+        this.dir = dir;
+        position = new Position(0, 0);
+        setPosition();
+    }
+
+    private void setPosition(){
+        switch(dir){
+            case DOWN:
+                position.setPosition(-region.getRegionWidth()/2, bottomLeft.getY());
+                break;
+            case UP:
+                position.setPosition(-region.getRegionWidth()/2,topRight.getY() - region.getRegionHeight());
+                break;
+            case LEFT:
+                position.setPosition(bottomLeft.getX(), bottomLeft.getY());
+                break;
+            case RIGHT:
+                position.setPosition(topRight.getX() - region.getRegionWidth(), bottomLeft.getY());
+                break;
+        }
+
+    }
+
+    public void draw(Batch batch){
+        batch.draw(region, position.getX(), position.getY(), region.getRegionWidth(), region.getRegionHeight());
+    }
+
+}
