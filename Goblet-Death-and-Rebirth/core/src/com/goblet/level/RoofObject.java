d92c6407af67e7fef4eef19b2c0b622acfad597d
Johan Ribom <johan.ribom@hotmail.com>
2016-05-24 23:44:30 +0200

    La till möjlighet att rita ut ett tak över dörrarna. Med taket så ser det snyggare ut när spelaren går ut genom en dörr.

---
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/level/RoofObject.java
@@ -0,0 +1,43 @@
+package com.goblet.level;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.goblet.entities.Direction;
+
+/**
+ * Created by Johan on 2016-05-24.
+ */
+public class RoofObject {
+
+    private TextureRegion region;
+    private Direction dir;
+    private Position position;
+    private Position bottomLeft;
+    private Position topRight;
+
+    public RoofObject(TextureRegion region, Direction dir, Position bottomLeft, Position topRight){
+        //Se vilken TextureRegion som ska användas i WallObject
+        this.region = region;
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+        this.dir = dir;
+        position = new Position(0, 0);
+        setPosition();
+    }
+
+    private void setPosition(){
+        switch(dir){
+            case LEFT:
+                position.setPosition(bottomLeft.getX(), bottomLeft.getY());
+                break;
+            case RIGHT:
+                position.setPosition(topRight.getX() - region.getRegionWidth(), bottomLeft.getY());
+                break;
+        }
+    }
+
+    public void draw(Batch batch){
+        batch.draw(region, position.getX(), position.getY(), region.getRegionWidth(), region.getRegionHeight());
+    }
+
+}
--- Goblet-Death-and-Rebirth/core/src/com/goblet/level/Room.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/level/Room.java
@@ -19,22 +19,29 @@ public class Room {
     String atlasLocation = "assets/tiles/room/room.pack";
     private TextureRegion floorTextureRegion;
     private Map<Direction, WallObject> wallMap;
+    private Map<Direction, RoofObject> roofMap;
     private TextureAtlas atlas;
     private Position bottomLeft;
     private Position topRight;
     private boolean doorsOpen;
 
 
-    public void draw(Batch batch){
-        batch.draw(floorTextureRegion, -floorTextureRegion.getRegionWidth()/2, bottomLeft.getY(), floorTextureRegion.getRegionWidth(), floorTextureRegion.getRegionHeight());
+    public void drawFloorAndWalls(Batch batch){
+        drawFloor(batch);
         for (WallObject currentWall : wallMap.values()){
             currentWall.draw(batch);
         }
 
     }
 
-    public void drawFloor(Batch batch){
+    private void drawFloor(Batch batch){
+        batch.draw(floorTextureRegion, -floorTextureRegion.getRegionWidth()/2, bottomLeft.getY(), floorTextureRegion.getRegionWidth(), floorTextureRegion.getRegionHeight());
+    }
 
+    public void drawRoof(Batch batch){
+        for (RoofObject currentRoof : roofMap.values()){
+            currentRoof.draw(batch);
+        }
     }
 
     public Room(float bottomLeftX, float bottomLeftY, float topRightX, float topRightY){
@@ -42,6 +49,7 @@ public class Room {
         topRight = new Position(topRightX, topRightY);
 
         wallMap = new HashMap<Direction, WallObject>();
+        roofMap = new HashMap<Direction, RoofObject>();
         tiles = new Tile[16][26];
         atlas = new TextureAtlas(Gdx.files.internal(atlasLocation));
 
@@ -49,6 +57,9 @@ public class Room {
         wallMap.put(Direction.DOWN, new WallObject(atlas.findRegion("spr_lower_wall"), atlas.findRegion("spr_lower_wall_path"), atlas.findRegion("spr_lower_door"), Direction.DOWN, bottomLeft, topRight, true));
         wallMap.put(Direction.LEFT, new WallObject(atlas.findRegion("spr_left_wall"), atlas.findRegion("spr_left_wall_path"), atlas.findRegion("spr_left_door"), Direction.LEFT, bottomLeft, topRight, true));
         wallMap.put(Direction.RIGHT, new WallObject(atlas.findRegion("spr_right_wall"), atlas.findRegion("spr_right_wall_path"), atlas.findRegion("spr_right_door"), Direction.RIGHT, bottomLeft, topRight, true));
+
+        roofMap.put(Direction.LEFT, new RoofObject(atlas.findRegion("spr_left_wall_path_roof"), Direction.LEFT, bottomLeft, topRight));
+        roofMap.put(Direction.RIGHT, new RoofObject(atlas.findRegion("spr_right_wall_path_roof"), Direction.RIGHT, bottomLeft, topRight));
 
         floorTextureRegion = atlas.findRegion("spr_floor");
     }
