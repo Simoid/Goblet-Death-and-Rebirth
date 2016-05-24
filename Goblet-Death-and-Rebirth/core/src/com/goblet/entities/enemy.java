7813361acdd3f7e5a775ca5c735384c122b8b02c
kawaji <kawaji@kth.se>
2016-05-24 20:16:37 +0200

    FIxade Enemy.java

--- Goblet-Death-and-Rebirth/core/src/com/goblet/entities/enemy.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/entities/enemy.java
@@ -7,41 +7,26 @@ import com.goblet.graphics.SpriteAnimation;
 import java.util.HashMap;
 
 /**
- * En klass för fienderna i spelet.
- *
- * Innehåller metoder för att röra sig mot spelarens position.
- *
  * Created by Johan on 2016-05-24.
  */
 public class Enemy extends Entity{
 
+    private Movement movement;
+    private HashMap<Direction, SpriteAnimation> animations = new HashMap<Direction, SpriteAnimation>();
+    private Hitbox hitbox;
 
-    /**
-     * Konstruktorn för fiendeklassen.
-     * @param xPos Startposition i x-led.
-     * @param yPos Startposition i y-led
-     * @param atlasLocation Pathen till filerna som fienden ska använda sig av.
-     * @param idleFrames Hur många frames fiendens animation har när den står stilla.
-     * @param moveFrames Hur många frames fiendens animation har när den rör sig.
-     * @param attackFrames Hur många frames fiendens animation har när den attackerar.
-     * @param movementSpeed Fiendens rörelsehastighet.
-     */
-    public Enemy(int xPos, int yPos, String atlasLocation, int idleFrames, int moveFrames, int attackFrames, float movementSpeed){
+    public Enemy(int xPos, int yPos, String atlasLocation, int idleFrames, int moveFrames, int attackFrames){
         super(xPos, yPos);
         animations.put(Direction.IDLE, new SpriteAnimation(spriteLocation + atlasLocation + "_idle.pack", idleFrames, 1.0f, 1.0f, 1f));
         animations.put(Direction.DOWN, new SpriteAnimation(spriteLocation + atlasLocation + "_walk.pack", moveFrames, 1.0f, 1.0f, 1/5f));
         animations.put(Direction.ATTACK, new SpriteAnimation(spriteLocation + atlasLocation + "_walk.pack", attackFrames, 1.0f, 1.0f, 1/5f));
-        movement = new Movement(movementSpeed);
+        movement = new Movement(50.0f);
         currentAnimation = animations.get(Direction.IDLE);
     }
 
-    /**
-     * Uppdaterar fienden och flyttar den mot spelaren.
-     * @param player Objektet för spelaren.
-     * @param deltaTime Tiden som har passerat sen senaste uppdateringen.
-     */
-    public void updateTowardsPlayer(Player player, float deltaTime){
+    public void update(Player player, float deltaTime){
         Position playerPosition = player.getPosition();
+        System.out.println("X: " + Math.abs(playerPosition.getX() - position.getX() + currentAnimation.getSpriteWidth()/2));
         if (Math.abs(playerPosition.getX() - (position.getX() + currentAnimation.getSpriteWidth()/2)) < 20.0f){
             movement.setMoveFlag(Direction.LEFT, false);
             movement.setMoveFlag(Direction.RIGHT, false);
@@ -52,21 +37,17 @@ public class Enemy extends Entity{
             movement.setMoveFlag(Direction.RIGHT, !moveLeft);
         }
         if (Math.abs(playerPosition.getY() - (position.getY() + currentAnimation.getSpriteHeight()/2)) < 20.0f){
-            movement.setMoveFlag(Direction.DOWN, false);
-            movement.setMoveFlag(Direction.UP, false);
+        movement.setMoveFlag(Direction.DOWN, false);
+        movement.setMoveFlag(Direction.UP, false);
         }
         else {
-            boolean moveDown = playerPosition.getY() < position.getY() + currentAnimation.getSpriteHeight()/2;
-            movement.setMoveFlag(Direction.DOWN, moveDown);
-            movement.setMoveFlag(Direction.UP, !moveDown);
+        boolean moveDown = playerPosition.getY() < position.getY() + currentAnimation.getSpriteHeight()/2;
+        movement.setMoveFlag(Direction.DOWN, moveDown);
+        movement.setMoveFlag(Direction.UP, !moveDown);
         }
-        selectAnimation();
         this.update(deltaTime);
     }
 
-    /**
-     * Väljer animation för fienden, beroende på i fall den attackerar, rör sig eller står still.
-     */
     public void selectAnimation(){
         if (movement.getMoveFlag(Direction.DOWN) || movement.getMoveFlag(Direction.LEFT) || movement.getMoveFlag(Direction.UP) || movement.getMoveFlag(Direction.RIGHT)){
             setAnimation(Direction.DOWN);
@@ -75,4 +56,18 @@ public class Enemy extends Entity{
         }
     }
 
+    private void setAnimation(Direction dir) {
+        if (currentAnimation != animations.get(dir)) {
+            currentAnimation = animations.get(dir);
+            timeSinceAnimationStart = 0;
+        }
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        selectAnimation();
+        timeSinceAnimationStart += deltaTime;
+        position.setPosition(position.getX() + deltaTime*movement.getMovementX(), position.getY() + deltaTime*movement.getMovementY());
+        //hitbox.updatePosition(position);
+    }
 }
