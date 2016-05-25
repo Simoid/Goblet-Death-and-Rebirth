5e46b84db9a633a20d7d29de0c47fad9432706d8
Johan Ribom <johan.ribom@hotmail.com>
2016-05-25 05:40:37 +0200

    Gjorde en första draft på Rock.java och (G)Obstacles.java.

--- Goblet-Death-and-Rebirth/core/src/com/goblet/level/GObstacles.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/level/GObstacles.java
@@ -1,5 +1,8 @@
 package com.goblet.level;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
@@ -8,7 +11,22 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public abstract class GObstacles {
 
     protected Position position;
+    protected float width;
+    protected float height;
+    protected Box hitbox;
+    protected TextureAtlas atlas;
     protected TextureRegion region;
-    //protected
+
+    public GObstacles(float posX, float posY, float width, float height, String atlasLocation){
+        this.position = new Position(posX, posY);
+        this.width = width;
+        this.height = height;
+        this.hitbox = new Box(position, width, height, 0, 0);
+        atlas = new TextureAtlas(Gdx.files.internal(atlasLocation));
+    }
+
+    public void draw(Batch batch){
+        batch.draw(region, position.getX(), position.getY());
+    }
 
 }
