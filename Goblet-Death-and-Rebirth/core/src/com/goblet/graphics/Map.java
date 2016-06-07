d903cc9b8c046e155ecc0ef23859ae86f460f58e
kawaji <kawaji@kth.se>
2016-06-07 21:11:37 +0200

    Uppdaterad så att kartans bakgrund är genomskynlig

--- Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/Map.java
+++ Goblet-Death-and-Rebirth/core/src/com/goblet/graphics/Map.java
@@ -7,7 +7,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.goblet.level.Floor;
+import com.goblet.level.FloorNode;
 import com.goblet.level.Position;
+
+import java.util.ArrayList;
 
 /**
  * Created by johan on 6/5/16.
@@ -20,9 +23,11 @@ public class Map {
     private TextureRegion mapBackgroundTexture;
     private Position middlePos;
     private Floor floor;
+    private ArrayList<FloorNode> visitedNodes;
 
 
     public Map(Position topRight, Floor floor){
+        visitedNodes = new ArrayList<FloorNode>();
         TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("assets/sprites/ui/map/Map.pack"));
         visitedRoomTexture = atlas.findRegion("map_visited_room");
         currentRoomTexture = atlas.findRegion("map_current_room");
@@ -39,7 +44,9 @@ public class Map {
     }
 
     private void drawBackground(Batch batch){
+        batch.setColor(1.0f,1.0f,1.0f,0.4f);
         batch.draw(mapBackgroundTexture, middlePos.getX() - mapBackgroundTexture.getRegionWidth()/2,middlePos.getY() - mapBackgroundTexture.getRegionHeight()/2);
+        batch.setColor(1.0f,1.0f,1.0f,1.0f);
     }
 
     private void drawFrame(Batch batch){
